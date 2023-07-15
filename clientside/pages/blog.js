c5b00f24438a8bea86db55661546ffa6b9f7cc6a
Niraj kumar chaurasiya <116571542+nirajchaurasiya@users.noreply.github.com>
2023-07-15 19:48:09 +0545

    updates

--- clientside/pages/blog.js
+++ clientside/pages/blog.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Blog from '../components/Blog/Blog'
 export default function blog() {
     return (
-        <div style={{}}>
+        <>
             <Blog />
-        </div>
+        </>
     )
 }
