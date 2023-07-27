4551f0c547b53faf50f960cc47d36dc767d70374
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2023-07-27 16:02:29 +0545

    updates

--- clientside/pages/project/Project.js
+++ clientside/pages/project/Project.js
@@ -2,42 +2,43 @@ import React from "react";
 import project from "./project.json";
 import { HiOutlineExternalLink } from "react-icons/hi";
 export default function Project(props) {
-  console.log(props.projectPos - 1);
-  console.log(project[props.projectPos - 1]);
   return (
     <>
       <div>
         <div className="project_description_contents">
           <div className="project_title_desc">
-            <p>{project[props.projectPos - 1].project_title}</p>
-            <p>{project[props.projectPos - 1].project_description}</p>
+            <p>{project[props.projectPos - 1]?.project_title}</p>
+            <p>{project[props.projectPos - 1]?.project_description}</p>
           </div>
 
           <div className="specific_project_image">
-            <img src={project[props.projectPos - 1].project_image_url} alt="" />
+            <img
+              src={project[props.projectPos - 1]?.project_image_url}
+              alt=""
+            />
           </div>
 
           <div className="specific_project_description">
             <h4>About</h4>
-            <p>{project[props.projectPos - 1].about}</p>
+            <p>{project[props.projectPos - 1]?.about}</p>
           </div>
           <br />
           <div className="specific_project_history">
             <h4>History</h4>
-            <p>{project[props.projectPos - 1].history}</p>
+            <p>{project[props.projectPos - 1]?.history}</p>
           </div>
           <br />
           <div className="specific_project_tech">
             <h4>Technologies used</h4>
             <div className="tech">
-              {project[props.projectPos - 1].technologies_used.map(
+              {project[props.projectPos - 1]?.technologies_used.map(
                 (e, index) => (
                   <div key={index} className="html">
                     <img
                       style={{ width: e.style.width, height: e.style.height }}
-                      src={e.image}
-                      alt={e.title}
-                      title={e.title}
+                      src={e?.image}
+                      alt={e?.title}
+                      title={e?.title}
                     />
                   </div>
                 )
