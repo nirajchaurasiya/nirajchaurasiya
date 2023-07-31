d585c3410b7c5ff59292fae9a25fffdb11934cf7
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2023-07-31 18:03:38 +0545

    updates

--- clientside/next.config.js
+++ clientside/next.config.js
@@ -36,31 +36,6 @@ module.exports = withOffline({
         webpack5: false, // Set webpack5: false here
       };
     }
-
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve("url-loader"),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve("file-loader"),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? "../" : ""}static/images/`,
-            name: "[name]-[hash].[ext]",
-            esModule: config.esModule || false,
-          },
-        },
-      ],
-    });
-
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag|svg)$/,
-      exclude: /node_modules/,
-      use: ["raw-loader", "glslify-loader"],
-    });
-
     return config;
   },
 });
