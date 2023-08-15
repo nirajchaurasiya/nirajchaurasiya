d344edb5c4682b61a7e7d1691d2d17dd24996604
Name Chaurasiya <116571542+nirajchaurasiya@users.noreply.github.com>
2023-08-15 07:42:46 +0545

    Update about.js

--- clientside/pages/about.js
+++ clientside/pages/about.js
@@ -52,35 +52,29 @@ export default function About() {
       </Head>
       <div className="_about_page">
         <div className="my_quick_story">
-          <p>My Quick Story</p>
+          <p>My Quick Story </p>
           <p>
-            I was an accomplished web developer, having completed a plethora of
-            web development projects. One particular project I worked on during
-            my college years in 2021 which involved building an online room
-            finder, which was aimed at solving the problem of finding rooms.
+            Hello there! I'm Niraj Kumar Chaurasiya, a web developer who loves crafting websites that look great and work smoothly. I've got a knack for taking complex code and turning it into user-friendly websites that people enjoy using. I've spent time mastering skills in frontend and backend, and I'm all about making websites that not only look cool but also make sense for the people who use them. It all started when I got curious about how the internet works, and now I get to create all sorts of things online. From online stores that make shopping a breeze to apps that solve everyday problems, I'm here to bring creative ideas to life using the latest techie stuff.
           </p>
           <p>
-            Initially, I was tasked with designing the user interface of the
-            website as a front-end developer. However, my colleagues had to
-            leave the project midway due to board exams, leaving me alone to
-            handle both front-end and back-end development.
+My very first project was an online room-finding application when I was in college. I was initially tasked with handling the front-end part, but due to our final exams, my friends left me in the middle of the journey. I had already completed the front-end part of the website, so I need to tackled the backend part anyhow to complete the project. However, I didn't know any backend since I was an front-end enginner. Since childhood, I have had the willingness within myself to achieve the goals I assign to myself. Hence, I found one back-end languge that suits my project. At last, I decided to go with PHP as it is one of the oldest languages in the world. 
+After learning PHP, I successfully completed the "Online Room Finder" project.
           </p>
           <p>
-            To tackle the challenge, I had to teach myself PHP, the programming
+            To tackle the challenge, I had to teach myself PHP, the programming language.
             language needed for back-end development. Despite encountering
             difficulties in debugging and coding, I persevered and eventually
-            succeeded in developing a fully functional website.
+succeeded in developing a fully functional website.
           </p>
           <p>
-            Overall, the experience taught me valuable lessons in adaptability
-            and problem-solving, and solidified my passion for web development.
+Overall, the experience taught me valuable lessons in adaptability and problem-solving and solidified my passion for web development. It helped me to know, "how to solve any problem in web development".
           </p>
         </div>
 
         <div className="as_a_web_developer my_quick_story">
-          <p>As A Web Developer</p>
+          <p>As A Web Developer </p>
           <p>
-            I pay close attention to my code&apos;s readability.I am a full
+I pay close attention to my code&apos;s readability.I am a full
             stack web developer, with a deep passion for creating dynamic and
             functional web applications. With expertise in both front-end and
             back-end development, I am able to bring a holistic approach to
