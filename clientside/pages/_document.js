1de0c885ba9759374a050e129cc696858fe02b90
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2025-07-10 00:44:23 -0500

    Update _document.js

--- clientside/pages/_document.js
+++ clientside/pages/_document.js
@@ -8,20 +8,20 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-W891NTYQT6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-W891NTYQT6');
-</script>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-W891NTYQT6"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-W891NTYQT6');
+          </script>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-
       </Html>
     );
   }
