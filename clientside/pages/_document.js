788196935fa1604ce3257c1fe2be95c0efc9f7e9
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2025-07-10 00:38:30 -0500

    Update _document.js

--- clientside/pages/_document.js
+++ clientside/pages/_document.js
@@ -13,11 +13,14 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5670755998255836"
-          crossOrigin="anonymous"
-        ></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-W891NTYQT6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-W891NTYQT6');
+</script>
       </Html>
     );
   }
