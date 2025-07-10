c5b01046d663cf72bb00c90503c676d313869c0c
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2025-07-10 00:47:15 -0500

    Update _document.js

--- clientside/pages/_document.js
+++ clientside/pages/_document.js
@@ -1,4 +1,6 @@
+// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -6,17 +8,23 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
-          <script
-            async
+
+          {/* ✅ Google Tag Manager */}
+          <Script
+            strategy="afterInteractive"
             src="https://www.googletagmanager.com/gtag/js?id=G-W891NTYQT6"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-W891NTYQT6');
-          </script>
+          />
+
+          <Script id="gtag-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W891NTYQT6');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
